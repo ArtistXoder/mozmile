94b9d8e4caf10de1914e30cac3415ae235e3876d
Garrett Goben <86270631+ArtistXoder@users.noreply.github.com>
2021-06-23 21:58:04 -0500

    Update software.sh

--- MinecraftScripts/software.sh
+++ MinecraftScripts/software.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 echo "Would you like to install all the software for MinecraftSever 1.17?"
-#included software Java opendk & Minecraft Server software & clamav & ufw
+#included software Java 16 openjdk & Minecraft Server software & clamav & ufw
 #FYI you need to accept and go into ELU and accept the agreement for the offical Minecraft Server Software
 sudo apt-get update
 sudo  apt-get upgrade
 cd ~
 mkdir MinecraftServer
 cd MinecraftServer
-sudo apt install default-jdk
+wget https://github.com/AdoptOpenJDK/openjdk16-binaries/releases/download/jdk16u-2021-05-08-12-45/OpenJDK16U-jdk_arm_linux_hotspot_2021-05-08-12-45.tar.gz
+tar xzf OpenJDK16U-jdk_arm_linux_hotspot_2021-05-08-12-45.tar.gz
+export PATH=$PWD/jdk-16.0.1+4/bin:$PATH
+java -version
 wget https://launcher.mojang.com/v1/objects/0a269b5f2c5b93b1712d0f5dc43b6182b9ab254e/server.jar
 sudo apt install clamav
 clamscan
