73db045814a58cabd1c8f0f74ff2bf924adcd797
Garrett <86270631+ArtistXoder@users.noreply.github.com>
2021-09-16 20:38:39 -0500

    Update software.sh

--- software.sh
+++ software.sh
@@ -17,7 +17,6 @@ do
  ;;
  esac
 done
-exit
 cd MinecraftServer
 wget https://github.com/AdoptOpenJDK/openjdk16-binaries/releases/download/jdk16u-2021-05-08-12-45/OpenJDK16U-jdk_arm_linux_hotspot_2021-05-08-12-45.tar.gz
 tar xzf OpenJDK16U-jdk_arm_linux_hotspot_2021-05-08-12-45.tar.gz
@@ -25,3 +24,4 @@ export PATH=$PWD/jdk-16.0.1+4/bin:$PATH
 java -version
 wget https://launcher.mojang.com/v1/objects/0a269b5f2c5b93b1712d0f5dc43b6182b9ab254e/server.jar
 java -Xmx2500M -Xms2500M -jar server.jar nogui
+exit 
