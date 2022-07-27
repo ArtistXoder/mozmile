2b98416570d2f41d50c481a86f12effee605eb24
Garrrett G <g.goben@outlook.com>
2022-07-26 20:56:39 -0500

    Update software.sh

--- software.sh
+++ software.sh
@@ -12,7 +12,6 @@ esac
 
 
 sudo apt-get update 
-sudo apt-get upgrade
 sudo apt install openjdk-17-jre-headless
 java -version
 wget https://launcher.mojang.com/v1/objects/e00c4052dac1d59a1188b2aa9d5a87113aaf1122/server.jar
