18c34bd90acf9061d01464698c11e2356a81c41b
Garrrett G <g.goben@outlook.com>
2022-07-27 13:43:29 -0500

    Update software.sh

--- software.sh
+++ software.sh
@@ -1,6 +1,7 @@
+
 #!/bin/bash
 
-read -r -p "Are you sure want to install Minecraft Server Software 1.19? [y/N] " input
+read -r -p "Are you sure want to install Minecraft Server Software 1.19? This will install noip & screen [y/N] " input
 case "$input" in
 	[yY][eE][sS]|[yY] | "")
 		;;
@@ -12,7 +13,17 @@ esac
 
 
 sudo apt-get update 
+sudo apt-get upgrade
 sudo apt install openjdk-17-jre-headless
 java -version
 wget https://launcher.mojang.com/v1/objects/e00c4052dac1d59a1188b2aa9d5a87113aaf1122/server.jar
+sudo aptr-get isntall screen 
+cd 
+mkdir noip
+cd noip
+wget https://www.noip.com/client/linux/noip-duc-linux.tar.gz
+tar vzxf noip-duc-linux.tar.gz
+cd noip-2.1.9-1
+sudo make
+sudo make install 
 
